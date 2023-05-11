446b57adda0b0dc64512878ad9d7346b251bff45
Nayeem <104270233+CaptaimNayeem@users.noreply.github.com>
2023-05-11 21:50:14 +0600

    something

--- admin.php
+++ admin.php
@@ -2,4 +2,38 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        < 
+        <meta http-equiv="X-UA-Compatiable" content="IE=edge">
+        <meta name="=viewport" content="width=device-width, initital-scale=1.0">
+        <tittle> admin page </tittle>
+
+        <!-- font awesome cdn link -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+        <!-- custom css file link -->
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+    <body>
+
+    <div class="container">
+
+    <section>
+        <form action=""method="post" class="add-product-form" enctype="multipart/form-data">
+            <h3> add a new product </h3>
+            <input type="text" name="p_name" placeholder="enter the product name" class="box" required>
+            
+        </form>
+    </section>
+    </div>
+
+
+
+
+
+
+    <!-- custom js file link -->
+    <script src="js/script.js"> </script>
+
+
+    </body>
+</html>
