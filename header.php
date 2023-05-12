b3f4cd3f92b6a25ca38b85c874f4ba6981b73d32
Nayeem <104270233+CaptaimNayeem@users.noreply.github.com>
2023-05-12 12:25:25 +0600

    s

---
+++ header.php
@@ -0,0 +1,17 @@
+<header class="header">
+<div class="flex">
+    <a href="#" class="logo">foofies</a>
+
+    <nav class="navbar">
+        <a href="admin.php">add product</a>
+        <a href="productss.php">view productss</a>
+    </nav>
+
+    <a href="cart.php">cart<span>0</span></a>
+
+
+</div>
+
+
+
+</header>
